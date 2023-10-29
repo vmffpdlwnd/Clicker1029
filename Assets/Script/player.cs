abcf1e5c69598189e24e935e99367658a1ace070
vmffpdlwnd <leegunehee@gmail.com>
2023-10-29 15:18:23 +0900

    10월 29일 2차 작업
    player랑 배경 추가
    player에 클릭효과와 코인생성 추가
    
    TODO
    UI 생성후 코인 pool로 추가

---
+++ Assets/Script/player.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public GameObject coinPrefab;
+    public float spawnRange = 2f;
+    public float forceUp = 5f;
+    public float coinLifetime = 10f;
+
+    private Vector3 originalScale;
+    private bool isScaling = false;
+    private Rigidbody coinRigidbody;
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+        if (coinPrefab != null)
+        {
+            coinRigidbody = coinPrefab.GetComponent<Rigidbody>();
+            if (coinRigidbody == null)
+            {
+                Debug.LogError("Rigidbody component not found on the 'coinPrefab' game object.");
+            }
+        }
+        else
+        {
+            Debug.LogError("'coinPrefab' is null.");
+        }
+    }
+
+    void Update()
+    {
+        if (!isScaling && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)))
+        {
+            StartCoroutine(SpawnCoin());
+            StartCoroutine(ScaleDown());
+        }
+    }
+
+    IEnumerator SpawnCoin()
+    {
+        // Player 앞에 랜덤한 위치 설정
+        Vector3 spawnPoint = transform.position + transform.right * Random.Range(-spawnRange, spawnRange) + transform.up * Random.Range(-spawnRange / 2, spawnRange / 2);
+        spawnPoint.y = Mathf.Max(spawnPoint.y, 2f); // y 좌표가 2 이상이 되도록 설정
+
+        // 코인 생성
+        GameObject spawnedCoin = Instantiate(coinPrefab, spawnPoint, Quaternion.identity);
+
+        if (coinRigidbody != null)
+        {
+            // 힘을 위로 가하기
+            coinRigidbody.AddForce(Vector3.up * forceUp, ForceMode.Impulse);
+        }
+
+        yield return new WaitForSeconds(coinLifetime);
+
+        // 코인 제거
+        Destroy(spawnedCoin);
+    }
+
+    IEnumerator ScaleDown()
+    {
+        isScaling = true;
+
+        // 축소
+        float elapsedTime = 0f;
+        float scaleDuration = 0.1f;
+        float scaleAmount = 0.9f;
+        Vector3 startScale = transform.localScale;
+        Vector3 targetScale = originalScale * scaleAmount;
+
+        while (elapsedTime < scaleDuration)
+        {
+            float t = elapsedTime / scaleDuration;
+            transform.localScale = Vector3.Lerp(startScale, targetScale, t);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // 원래 크기로 복원
+        transform.localScale = originalScale;
+        isScaling = false;
+    }
+}
