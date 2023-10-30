bb150d091feb3b72c105cb3c219b3e4fc92a5595
vmffpdlwnd <leegunehee@gmail.com>
2023-10-31 00:19:54 +0900

    10월 30일 3차 작업
    ControlSky 코드 생성
    시간에 따라 낮과 밤이 바뀌는 효과와 낮에는 Directional Light도 움직이는 효과 추가
    
    Todo
    Scene 2개 추가

---
+++ Assets/Script/ControlSky.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class ControlSky : MonoBehaviour
+{
+    public Material dayMat;
+    public Material nightMat;
+
+    public Light directionalLight;
+    private Quaternion originalRotation;
+
+    private void Start()
+    {
+        originalRotation = directionalLight.transform.rotation;
+    }
+
+    private void Update()
+    {
+        float currentTime = Time.time;
+        float rotation = currentTime * 1f % 120f; // rotation is always between 0 and 360.
+        UnityEngine.RenderSettings.skybox.SetFloat("_Rotation", rotation);
+
+        
+
+        if (rotation <= 60f)
+        {
+            UnityEngine.RenderSettings.skybox = dayMat;
+            directionalLight.enabled = true;
+            float rotationSpeed = 2f; // Define the rotation speed.
+            directionalLight.transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            UnityEngine.RenderSettings.skybox = nightMat;
+            directionalLight.enabled = false;
+
+            directionalLight.transform.rotation = originalRotation;
+        }
+
+        Debug.Log(currentTime);
+
+    }
+}
