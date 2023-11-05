28afef090e9e949f0531e379445d59dcb2975b72
vmffpdlwnd <leegunehee@gmail.com>
2023-11-05 21:15:17 +0900

    11월 5일 5차 작업
    유니티 오류로 인하 작업

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -36,6 +36,8 @@ public class PlayerData
 
 public class GameManager : Singleton<GameManager>
 {
+    float rotation;
+
     private PlayerData pData = new PlayerData();
     public PlayerData PlayerInfo
     {
@@ -80,6 +82,23 @@ public class GameManager : Singleton<GameManager>
     #region _Save&Load_
 
     private string dataPath; //= Application.persistentDataPath + "/save"; // 디바이스 정해진 폴더에 save폴더 추가
+
+    void Update()
+    {
+        if (Time.time >= rotation)
+        {
+            AutoSave();
+            rotation = Mathf.Ceil(Time.time) + 60f; // 다음 저장 시간을 현재 시간에서 60초 뒤로 재설정
+        }
+    }
+
+    void AutoSave()
+    {
+        Debug.Log("Game Saved!");
+        // 실제 게임 저장 로직을 여기에 구현하십시오.
+        string data = JsonUtility.ToJson(pData); // 객체정보를 string타입으로 변경 ( Json 구조를 활용 )
+        File.WriteAllText(dataPath, data);
+    }
 
     public void SaveData()
     {
@@ -162,23 +181,6 @@ public class GameManager : Singleton<GameManager>
     {
         pData.gold += addGold * pData.level;
         UIManager.Instance.UpdateUI();
-    }
-
-    #endregion
-
-    #region _AutoSave_
-
-    private float autoSaveInterval = 60f; // 자동 저장 간격 (초 단위)
-    private float lastSaveTime = 0f; // 마지막 저장 시간
-
-    private void Update()
-    {
-        if (Time.time - lastSaveTime >= autoSaveInterval)
-        {
-            Debug.Log("Save Data!");
-            SaveData();
-            lastSaveTime = Time.time;
-        }
     }
 
     #endregion
