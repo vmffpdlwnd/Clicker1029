af963e6b9791288f2ee3fb79f5a71ce9d9e85585
vmffpdlwnd <leegunehee@gmail.com>
2023-11-05 22:03:19 +0900

    11월 5일 7차 작업
    지금까지 일어났던 유니티 오류 확인후 수정
    
    Todo
    State 내용 수정
    Popup 올라온게 있으면 다른것 못올라오게 하기
    계정 삭제시 코인 남아있는 오류 수정

--- Assets/Script/Manager/Singleton.cs
+++ Assets/Script/Manager/Singleton.cs
@@ -7,14 +7,14 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     private static T instance = null;
     public static T Instance
     {
-        get
+        get // 인스턴스를 참조 하려고 할때 인스턴스가 없으면, 신규 생성하여 리턴.
         {
             if (instance == null)
             {
-                instance = FindObjectOfType<T>();
-                if (instance == null)
+                instance = FindObjectOfType<T>(); // 씬내에 동일한 타입의 오브젝트가 있는지 찾음.
+                if (instance == null) // 찾기 실패
                 {
-                    GameObject obj = new GameObject(typeof(T).Name, typeof(T));
+                    GameObject obj = new GameObject(typeof(T).Name, typeof(T)); // T 타입의 오브젝트를 신규 생성.
                     instance = obj.GetComponent<T>();
                 }
             }
@@ -22,19 +22,15 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
         }
     }
 
-    private static bool isInitialized = false;
-    public static void Initialize()
+    public void Awake()
     {
-        if (!isInitialized)
+        if (transform.parent != null & transform.root != null) // 해당 오브젝트가 부모를 가지고 있는 오브젝트라면
         {
-            if (instance is MonoBehaviour monoBehaviourInstance)
-            {
-                monoBehaviourInstance.gameObject.name = typeof(T).Name;
-                DontDestroyOnLoad(monoBehaviourInstance.gameObject);
-            }
-            isInitialized = true;
+            DontDestroyOnLoad(this.transform.root.gameObject); // 부모까지 DontDestroy 오브젝트로 설정.
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject); // 자기 자신만 DontDestroy 오브젝트로 설정.
         }
     }
-
-
 }
